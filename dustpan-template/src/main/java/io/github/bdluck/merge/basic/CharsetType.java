c3b2a38c0f2f672b596ea536642b5499bf6875b6
Pan.Liang <panliang@jimilab.com>
2021-04-30 18:10:10 +0800

    版本1.0.3 注释修改

--- dustpan-template/src/main/java/io/github/bdluck/merge/basic/CharsetType.java
+++ dustpan-template/src/main/java/io/github/bdluck/merge/basic/CharsetType.java
@@ -4,10 +4,28 @@ package io.github.bdluck.merge.basic;
  * @author bdluck
  */
 public enum CharsetType {
+    /**
+     * ASCII
+     */
     US_ASCII,
+    /**
+     * ISO_8859_1
+     */
     ISO_8859_1,
+    /**
+     * UTF-8
+     */
     UTF_8,
+    /**
+     * UTF_16BE
+     */
     UTF_16BE,
+    /**
+     * UTF_16LE
+     */
     UTF_16LE,
+    /**
+     * UTF_16
+     */
     UTF_16
 }
